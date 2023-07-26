e8ddbbc19a38dfd35261db4642c5085967bb9246
nan cai <nora.cainan11@gmail.com>
2023-07-26 10:05:19 +0200

    two trajactory finish, but very close to each other

--- crazyflie_examples/crazyflie_examples/data_test.py
+++ crazyflie_examples/crazyflie_examples/data_test.py
@@ -2,19 +2,18 @@
 
 import numpy as np
 from pathlib import Path
-
-from crazyflie_py import *
-# from crazyflie_py.uav_trajectory import Trajectory
+import matplotlib.pyplot as plt
 import yaml
+
+# from crazyflie_py import *
+# # from crazyflie_py.uav_trajectory import Trajectory
+# import yaml
 
 '''
 want to see the trajectory cause it is now not that round enough
 save the xyz format and use uav gen to visualise it 
 '''
 def main():
-
-
-
     # load yaml file contains smooth waypoint
     yaml_path = Path(__file__).parent / "data/result_ompl.yaml"
     with open(yaml_path, 'r') as ymlfile:
@@ -22,15 +21,34 @@ def main():
     print('load finish')
     
     
-    height = 0.5
+    # height = 0.5
     n = len(data) # number of distinct trajectories
     traj1 = data[0]
-    states = traj1['states']  # lisrt len 73
-    print(states)  # list 
-    print('end')
+    states = traj1['states']  # list len 73
+    # print(np.array(states))  # list 
+    traj2 = data[1]
+    states2 = traj2['states']  # list len 73
+    print('states2',states2)
 
 
+    number = 70
+    # Extract x and y coordinates from the points
+    x_coords1 = [point[0] for point in states[:number]]
+    y_coords1 = [point[1] for point in states[:number]]
+    x_coords2 = [point[0] for point in states2[:number]]
+    y_coords2 = [point[1] for point in states2[:number]]
+    # Plot the points
+    # Plot the first set of points in blue
+    plt.scatter(x_coords1, y_coords1, color='blue', label='States 1')
+    # Plot the second set of points in red
+    plt.scatter(x_coords2, y_coords2, color='red', label='States 2')  # [0.762263, 1.01913]
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Scatter Plot of Points')
+    plt.grid(True)
+    plt.show()
 
+    # print('end')
 
 if __name__ == "__main__":
     main()
--- crazyflie_examples/crazyflie_examples/round.py
+++ crazyflie_examples/crazyflie_examples/round.py
@@ -7,45 +7,116 @@ from crazyflie_py import *
 # from crazyflie_py.uav_trajectory import Trajectory
 import yaml
 
+height = 0.5
 '''
 the first states is the position
 the last is the x y way point
 '''
-def main():
+def one_traj():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs  # CrazyflieServer
-
-    allcfs.takeoff(targetHeight=0.5, duration=2.0)
-    timeHelper.sleep(2)
 
     # load yaml file contains smooth waypoint
     yaml_path = Path(__file__).parent / "data/result_ompl.yaml"
     with open(yaml_path, 'r') as ymlfile:
         data = yaml.safe_load(ymlfile)['result']  # a list  elements are dictionaries
     print('load finish')
-    
-    
-    height = 0.5
-    n = len(data) # number of distinct trajectories
+
     traj1 = data[0]
     states = traj1['states']  # list len 73
-    # print(type(states))  # list 
+
+    allcfs.takeoff(targetHeight=1.0, duration=2.0)
+    timeHelper.sleep(2.5)
+
+    #initial position
+    pos = np.append(np.array(states[0]), height)
+    print('pos',pos)
     for cf in allcfs.crazyflies:
-        pos = np.append(np.array(states[0]), height)
-        print('pos',pos)
         cf.goTo(pos, 0, 2.0)  # goal yaw duration
+    # timeHelper.sleep(2)
+
     for state in states[1:]:  # state list
         print('state:',state)
         pos = np.append(np.array(state), height)
         for cf in allcfs.crazyflies:
-            cf.goTo(pos, 0, 5.0)  # goal yaw duration
-        # timeHelper.sleep(2)
+            cf.goTo(pos, 0, 6.0)  # goal yaw duration
         print('1')
 
-    # timeHelper.sleep(2)
-    # allcfs.land(targetHeight=0.06, duration=2.0)
+    timeHelper.sleep(5)
+    allcfs.land(targetHeight=0.06, duration=2.0)
 
+def multi_traj():
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    allcfs = swarm.allcfs  # CrazyflieServer
+
+    # load yaml file contains smooth waypoint
+    yaml_path = Path(__file__).parent / "data/result_ompl.yaml"
+    with open(yaml_path, 'r') as ymlfile:
+        data = yaml.safe_load(ymlfile)['result']  # a list  elements are dictionaries
+    print('load finish')
+
+    height = 0.5
+    n = len(data) # number of distinct trajectories
+    states_list = []
+    for i in range(n):
+        states = data[i]['states']
+        states_list.append(states)
+    # print('states_list',states_list)
+
+    allcfs.takeoff(targetHeight=0.5, duration=2.0)
+    timeHelper.sleep(2)
+    print('len(states_list[0])',len(states_list[0]))
+    for state_id in range(len(states_list[0])):  # 73  can goto synchronized for multi drones?
+        for drone_id in range(len(allcfs.crazyflies)): # 2
+            togo_time = 10.0
+            if state_id ==1:
+                pos = np.append(np.array(states_list[drone_id][state_id]), height)
+                allcfs.crazyflies[drone_id].goTo(pos, 0, togo_time)
+                timeHelper.sleep(togo_time/2)
+            else:
+
+                pos = np.append(np.array(states_list[drone_id][state_id]), height)
+                # print(states_list[drone_id])
+                print('drone_id',drone_id,'state_id',state_id)
+                print(len(allcfs.crazyflies))
+                allcfs.crazyflies[drone_id].goTo(pos, 0, togo_time)
+            # timeHelper.sleep(1)
+
+
+    timeHelper.sleep(togo_time + 1)
+    allcfs.land(targetHeight=0.06, duration=2.0)
+
+
+    quit()
+
+
+
+    traj1 = data[0]
+    states = traj1['states']  # list len 73
+    # print(type(states))  # list 
+    #initial position
+    pos = np.append(np.array(states[0]), height)
+    print('pos',pos)
+    for cf in allcfs.crazyflies:
+        cf.goTo(pos, 0, 2.0)  # goal yaw duration
+    timeHelper.sleep(2)
+
+    for state in states[1:]:  # state list
+        print('state:',state)
+        pos = np.append(np.array(state), height)
+        for cf in allcfs.crazyflies:
+            cf.goTo(pos, 0, 6.0)  # goal yaw duration
+        print('1')
+
+    timeHelper.sleep(5)
+    allcfs.land(targetHeight=0.06, duration=2.0)
+
+
+def main():
+    # one_traj()
+    multi_traj()
 
 if __name__ == "__main__":
     main()
